44cb19458ccad04da342f33c8b44bf728a6a255a
Ju-Chi Yu <23201134+juchiyu@users.noreply.github.com>
2021-03-11 18:52:06 +0800

    Initialize sMCA and sPLSCA functions

---
+++ R/sMCA.R
@@ -0,0 +1,68 @@
+#' Group sparse Correspondence Analysis
+#'
+#' @param DATA the contingency table
+#' @param k the number of dimensions
+#' @param tol
+#' @param doublecentering
+#' @param init
+#' @param initLeft
+#' @param initRight
+#' @param seed
+#' @param rdsLeft
+#' @param rdsRight
+#' @param grpLeft
+#' @param grpRight
+#' @param orthogonality
+#' @param OrthSpaceLeft
+#' @param OrthSpaceRight
+#' @param projPriority
+#' @param projPriorityLeft
+#' @param projPriorityRight
+#' @param itermaxALS
+#' @param itermaxPOCS
+#' @param epsALS
+#' @param epsPOCS
+#'
+#' @return
+#' @export
+#'
+#' @examples
+sparseMCA <- function(
+  DATA, k = 0, tol = .Machine$double.eps,
+  doublecentering = TRUE,
+  init = "svd", initLeft = NULL, initRight = NULL, seed = NULL,
+  rdsLeft = rep(1, R), rdsRight = rep(1, R),
+  grpLeft = NULL, grpRight = NULL,
+  orthogonality = "loadings",
+  OrthSpaceLeft = NULL, OrthSpaceRight = NULL,
+  projPriority = "orth",
+  projPriorityLeft = projPriority,
+  projPriorityRight = projPriority,
+  itermaxALS = 1000, itermaxPOCS = 1000,
+  epsALS = 1e-10, epsPOCS = 1e-10) {
+
+
+  mRP <- ExPosition::makeRowProfiles(DATA, weights = NULL, masses = NULL, hellinger = FALSE)
+  # N <- sum(DATA)
+  # X <- 1/N * DATA
+  # Lv <- rowSums(X)
+  # Rv <- colSums(X)
+  # if (doublecentering) X <- X - Lv %*% t(Rv)
+  # LW <- 1/sqrt(Lv)
+  # RW <- 1/sqrt(Rv)
+
+  res.spgsvd <- sparseGSVD(mRP$deviations, LW = mRP$masses, RW = mRP$weights, k = k, tol = .Machine$double.eps,
+                           init = init, initLeft = initLeft, initRight = initRight, seed = NULL,
+                           rdsLeft = rdsLeft, rdsRight = rdsRight,
+                           grpLeft = grpLeft, grpRight = grpRight,
+                           orthogonality = orthogonality,
+                           OrthSpaceLeft = OrthSpaceLeft,
+                           OrthSpaceRight = OrthSpaceRight,
+                           projPriority = projPriority,
+                           projPriorityLeft = projPriorityLeft,
+                           projPriorityRight = projPriorityRight,
+                           itermaxALS = itermaxALS, itermaxPOCS = itermaxPOCS,
+                           epsALS = epsALS, epsPOCS = epsPOCS)
+
+
+}
---
+++ R/sPLSCA.R
@@ -0,0 +1,55 @@
+#' Group sparse Correspondence Analysis
+#'
+#' @param DATA the contingency table
+#' @param k the number of dimensions
+#' @param tol
+#' @param doublecentering
+#' @param init
+#' @param initLeft
+#' @param initRight
+#' @param seed
+#' @param rdsLeft
+#' @param rdsRight
+#' @param grpLeft
+#' @param grpRight
+#' @param orthogonality
+#' @param OrthSpaceLeft
+#' @param OrthSpaceRight
+#' @param projPriority
+#' @param projPriorityLeft
+#' @param projPriorityRight
+#' @param itermaxALS
+#' @param itermaxPOCS
+#' @param epsALS
+#' @param epsPOCS
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+sparsePLSCA <- function(DATA, k = 0, tol = .Machine$double.eps,
+                        doublecentering = TRUE,
+                        init = "svd", initLeft = NULL, initRight = NULL, seed = NULL,
+                        rdsLeft = rep(1, R), rdsRight = rep(1, R),
+                        grpLeft = NULL, grpRight = NULL,
+                        orthogonality = "loadings",
+                        OrthSpaceLeft = NULL, OrthSpaceRight = NULL,
+                        projPriority = "orth",
+                        projPriorityLeft = projPriority,
+                        projPriorityRight = projPriority,
+                        itermaxALS = 1000, itermaxPOCS = 1000,
+                        epsALS = 1e-10, epsPOCS = 1e-10){
+  sparseGSVD(X, LW, RW, k = 0, tol = .Machine$double.eps,
+             init, initLeft = NULL, initRight = NULL, seed = NULL,
+             rdsLeft = rep(1, R), rdsRight = rep(1, R),
+             grpLeft = NULL, grpRight = NULL,
+             orthogonality = "loadings",
+             OrthSpaceLeft = NULL, OrthSpaceRight = NULL,
+             projPriority = "orth",
+             projPriorityLeft = projPriority,
+             projPriorityRight = projPriority,
+             itermaxALS = 1000, itermaxPOCS = 1000,
+             epsALS = 1e-10, epsPOCS = 1e-10)
+
+}
