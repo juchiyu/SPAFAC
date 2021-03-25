769e9146a6f2e9779bd0da4f179f04b80219cec6
Ju-Chi Yu <23201134+juchiyu@users.noreply.github.com>
2021-03-25 16:32:52 +0800

    Build the package -- stil got 2 errors

--- tests/testthat/test-PLScPLSca.R
+++ tests/testthat/test-PLScPLSca.R
@@ -7,4 +7,4 @@ library(GPLS)
 data("wine", package = "GSVD")
 plscor_results <- pls_cor(wine$objective, wine$subjective, components = 2)
 
-spls.res <- sparsePLSC(wine$objective, wine$subjective, components = 2L, rdsLeft = rep(sqrt(ncol(wine$objective)), 2L), rdsRight = rep(sqrt(ncol(wine$subjective)), 2L))
+spls.res <- sparsePLSC(wine$objective, wine$subjective, orthogonality = "loadings", components = 2L, rdsLeft = rep(sqrt(ncol(wine$objective)), 2L), rdsRight = rep(sqrt(ncol(wine$subjective)), 2L))
