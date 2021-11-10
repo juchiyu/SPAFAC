1b0b7c67c3d61638433ead3ec8bf877d6d2fefaf
Ju-Chi Yu <23201134+juchiyu@users.noreply.github.com>
2021-11-10 10:50:01 -0500

    Change warnings to messages, add grpRight to test-DiCA.R

--- tests/testthat/test-Dica.R
+++ tests/testthat/test-Dica.R
@@ -21,6 +21,7 @@ X.dica.proc <- X.dica.proc - Lv %*% t(Rv)
 LW <- 1/Lv
 RW <- 1/Rv
 
+grpRight.constraint <- sub("\\..+","", colnames(X.data))
 
 # run DiCA
 dica.res <- tepDICA(dica.wine$data,DESIGN=dica.wine$design,make_design_nominal=FALSE, graphs = FALSE)
@@ -29,7 +30,7 @@ gsvd.wine.res <- gsvd(as.matrix(X.dica.proc), LW = diag(LW), RW = diag(RW))
 # run sGSVD
 sGSVD.res <- sparseGSVD(as.matrix(X.dica.proc), LW = diag(LW), RW = diag(RW), k = 2L, init = "svd", rdsLeft = rep(sqrt(nrow(X.dica)), 2), rdsRight = rep(sqrt(ncol(X.dica)), 2))
 # run sDiCA
-sdica.wine.res <- sparseDiCA(as.matrix(X.data), design = X.des, make_data_nominal = FALSE, make_design_nominal = FALSE, components = 2L, rdsLeft = rep(sqrt(nrow(X.dica)), 2), rdsRight = rep(sqrt(ncol(X.dica)), 2))
+sdica.wine.res <- sparseDiCA(as.matrix(X.data), design = X.des, make_data_nominal = FALSE, make_design_nominal = FALSE, components = 2L, rdsLeft = rep(sqrt(nrow(X.dica)), 2), rdsRight = rep(sqrt(ncol(X.dica)), 2), grpRight = grpRight.constraint)
 
 test_that("sparseDiCA with no sparsification gives back plain DiCA", {
   expect_equal(abs(sdica.wine.res$gsvd$p), abs(dica.res$TExPosition.Data$M * dica.res$TExPosition.Data$pdq$p), tolerance = tol)
