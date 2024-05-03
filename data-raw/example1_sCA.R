2cbcc6b58d6f9850eb0c60334f85951e8d627dc6
Vincent  GUILLEMOT <vincent.guillemot@pasteur.fr>
2024-05-03 17:36:50 +0200

    UPDATE example 1

--- data-raw/example1_sCA.R
+++ data-raw/example1_sCA.R
@@ -1,6 +1,6 @@
 ## code to prepare `example1_sCA` dataset goes here
 
 load("death.2019.rda")
-example1_sCA <- death.2019
+example1_sCA <- death.2019[-c(3, 8, 15, 19), ]
 
 usethis::use_data(example1_sCA, overwrite = TRUE)
